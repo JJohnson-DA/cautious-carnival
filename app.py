e056a6ccca33c040680baa09a8bd61d22577c373
Jared-Johnson <jaj53223@bethel.edu>
2022-06-30 08:58:57 -0500

    Change app to be only from user text input.

--- app.py
+++ app.py
@@ -1,66 +1,70 @@
 import streamlit as st
-import pandas as pd
-
-
-players = pd.read_csv("players.csv")
+from random import shuffle
 
 st.markdown("# Let's Pick Some Teams.")
 
 with st.form("form"):
     st.subheader("Who's Playing?")
-    all_players = st.checkbox("Use All Players", value=True)
+
+    # Get player names from user
     chosen_players_text = st.text_input(
-        "OR...",
+        "",
         placeholder="Enter names seperated by commas (ex. Scott, Adam, Lainy, Ryan).",
     )
+
+    # Split names into list
     chosen_players = [x.strip() for x in chosen_players_text.split(",")]
-    # chosen_players = st.multiselect(label="Select Players", options=players["Name"])
 
-    if all_players:
-        temp = players.shape[0]
-    else:
-        temp = len(chosen_players)
+    # select_opts = list(
+    #     filter(
+    #         None, [x if num_players % x == 0 else None for x in range(2, num_players)]
+    #     )
+    # )
 
-    select_opts = list(
-        filter(None, [x if temp % x == 0 else None for x in range(2, temp)])
-    )
     st.subheader("How Many Teams?")
 
+    # Get number of teams from user
     number_of_teams = int(
         st.select_slider(
             "",
             value=2,
-            options=[1, 2, 3, 4, 5, 6],
+            options=[
+                1,
+                2,
+                3,
+                4,
+                5,
+                6,
+            ],  # this needs to be swapped for dynamically created list
             help="Select 1 team to get a randomized order of all player names.",
         ),
     )
     submit = st.form_submit_button("Create Teams")
 
 if submit:
-    if all_players:
-        randomized = players.sample(n=players.shape[0]).reset_index(drop=True)
+    # Randomize order of players
+    shuffle(chosen_players)
 
-    else:
-        randomized = (
-            players[[x in chosen_players for x in players.Name]]
-            .sample(n=len(chosen_players))
-            .reset_index(drop=True)
-        )
-
-    if randomized.shape[0] % number_of_teams != 0:
+    # Check that teams would be even numbers
+    if len(chosen_players) % number_of_teams != 0:
         st.warning(
-            "Players per team is not even, add more players or lower the number of teams."
+            "Players per team is not even, add more players or change the number of teams."
         )
     else:
-        players_per_team = randomized.shape[0] // number_of_teams
+        # Determine players per team
+        players_per_team = len(chosen_players) // number_of_teams
 
+        # Create column structure to display teams
         cols = st.columns(number_of_teams)
 
+        # loop through players
         for i in range(1, number_of_teams + 1):
+            # Iterate over the correct column
             with cols[i - 1]:
-                subset = randomized.iloc[:players_per_team]
-                randomized = randomized.drop(range(0, players_per_team)).reset_index(
-                    drop=True
-                )
+                # Slice off the first n players
+                subset = chosen_players[0:players_per_team]
+                # Trim down the player list
+                chosen_players = chosen_players[players_per_team:]
+                # Display Team
                 st.subheader(f"Team {i}")
-                st.write(", ".join([str(x) for x in subset.Name]))
+                st.write(", ".join([str(x) for x in subset]))
